0ad6ccc3bb49e21d1ec704ad1c329dadada4b07f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-09 18:43:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="container">
-    <Header  title="Hello" name="Balou" age="38" />
+    <Header />
     </div>
   );
 }
